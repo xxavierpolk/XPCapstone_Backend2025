a9f267c60c56f3215716ce16586b6e63203c9d53
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-22 19:24:29 -0500

    Error Handle for post users work

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -29,7 +29,7 @@ router.post('/', [
     const { name, email, password } = req.body;
 
     try {
-        
+        // 
     } catch (error) {
         console.error(error)
         res.status(500).json({ errors: [{ msg: 'Server Error' }] });
