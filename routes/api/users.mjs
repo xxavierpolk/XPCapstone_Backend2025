42809b715b0d5db570a45f92e80ad2496c9a1905
Xxavier Polk <xxavier@xxavierpolk.com>
2025-02-22 19:03:23 -0500

    Express Val Setup

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -9,6 +9,33 @@ const router = express.Router();
 // @route:   POST api/users
 // @desc:    Register User Route
 // @access:  Public
-router.post('/', async (req, res) => res.send('User Route'));
+
+// Creating a new user with validation
+router.post('/', [
+    check ('name', 'Name is required').not().isEmpty(),
+    check ('email', 'Please include a valid email').isEmail(),
+    check ('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+], async (req, res) => {
+    // Check for any validation errors in the request object
+    // creates an array of errors and checking the request 
+    const errors = validationResult(req);
+
+    // If the errors variables is NOT empty, response with errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure the request body / req.body
+    const { name, email, password } = req.body;
+
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ errors: [{ msg: 'Server Error' }] });
+    }
+    
+
+});
 
 export default router;
